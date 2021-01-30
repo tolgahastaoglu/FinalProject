d8087758006164d9623332ef24d0a2f724d77e73
tolga hastaoğlu <tolga_hastaoglu@hotmail.com>
2021-01-31 00:05:23 +0300

    Add project files.

---
+++ Entities/Abstract/IEntity.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Entities.Abstract
+{
+    //IEntity implement eden Class bir veritabanı tablosudur
+  public interface IEntity
+    {
+
+    }
+}
